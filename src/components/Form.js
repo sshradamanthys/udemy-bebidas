7ff7241c19e0aac77e68b1d3db7f3eb1da1b9f65
Denis Valdivieso <sshshaka@gmail.com>
2021-11-19 09:12:06 -0400

    + inputs - Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,32 @@
 const Form = () => {
   return (
-    <div>
-      <h2>Formulario</h2>
-    </div>
+    <form className="col-12">
+      <fieldset className="text-center">
+        <legend>Busca bebidas por Categoría o Ingrediente</legend>
+      </fieldset>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            placeholder="Buscar por Ingrediente"
+          />
+        </div>
+        <div className="col-md-4">
+          <select name="category" id="" className="form-control">
+            <option value="">--- Selecciona Categoría ---</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            value="Buscar Bebidas"
+            className="btn btn-primary btn-block"
+          />
+        </div>
+      </div>
+    </form>
   );
 };
 
