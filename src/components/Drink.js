832bde142e83ff3719f93d84529b081084b49b4c
Denis Valdivieso <sshshaka@gmail.com>
2021-11-24 11:55:10 -0400

    testing Modal

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -1,8 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
+
+import { makeStyles } from "@mui/styles";
+import Modal from "@mui/material/Modal";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 450,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+console.log(useStyles);
 
 const Drink = ({ drink }) => {
   const { setId } = useContext(ModalContext);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <div className="col-md-4 mb-3">
@@ -18,10 +37,14 @@ const Drink = ({ drink }) => {
             className="btn btn-primary btn-block"
             onClick={() => {
               setId(drink.idDrink);
+              handleOpen();
             }}
           >
             Ver Receta
           </button>
+          <Modal open={open} onClose={handleClose}>
+            <h3>Soy un modal</h3>
+          </Modal>
         </div>
       </div>
     </div>
